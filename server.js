b0b8f3b8ef1ffd84547e757be72bb89fc91ade13
mickamix <mickey_mix@live.com>
2019-04-22 17:55:05 +0700

    change port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Start server at port 3000.')
 })
 
